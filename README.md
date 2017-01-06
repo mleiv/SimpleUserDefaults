e8018b631ffe6e7ce597564c5e7f64db51846dc8
Emily Ivie <mleiv@yahoo.com>
2017-01-06 13:05:35 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # SimpleUserDefaults
-Easy interface to UserDefaults (Swift 3.0)
+
+A generally type-agnostic, highly simplified way to access/cache UserDefaults using Swift 3.
+
+Before you try this type, I highly recommend looking at https://github.com/radex/SwiftyUserDefaults first. Since you probably want that instead. I just found that I preferred encapsulated object property access for my defaults-stored data. And it was a fun experiment.
+
+### How It Works
+
+1. Establish your property value type and name:
+```swift
+var myStoredPreference = SimpleUserDefaults<String>(name: "myStoredPreference",  defaultValue: "none")
+```
+(The **name** parameter should be unique across your application.)
+
+2. Access your property:
+```swift
+if myStoredPreference.get() == "happy" {
+    // do something
+}
+```
+
+3. Store your property:
+```swift
+myStoredPreference.set("notHappy")
+```
+
+### Suggested Extensions
+
+1. I'd recommend extending SimpleUserDefaults.init() and get() to accept your own enum type, and use that to define the keys.
+
+2. Improved access.
+I just left it with get()/set() access, so I wouldn't have to create ExpressibleXXXXXXLiteral protocols for all my custom types and assign SimpleUserDefaults to use them.
+But here is a way to improve access with a bit more declaration code. If you want to make them look exactly like regular properties or stuff.
+```swift
+private var _myStoredPreference = SimpleUserDefaults<String>(name: "myStoredPreference",  defaultValue: "none")
+var myStoredPreference: String {
+    get{ return _myStoredPreference.get() } 
+    set{ _myStoredPreference.set(newValue) }
+}
+//...
+if myStoredPreference == "happy" {
+    myStoredPreference = "stillhappy"
+}
+```
---
+++ SimpleUserDefaults.swift
@@ -0,0 +1,140 @@
+//
+//  SimpleUserDefaults.swift
+//
+//  Created by Emily Ivie on 1/6/17.
+//  Copyright © 2017 Emily Ivie. 
+//
+//  Licensed under The MIT License
+//  For full copyright and license information, please see http://opensource.org/licenses/MIT
+//  Redistributions of files must retain the above copyright notice.
+//
+
+import Foundation
+
+/// A simplified way to get/set user defaults.
+///
+/// Accepts pretty much anything. Always allows and returns an *optional* value.
+///
+/// Note: **name** parameter should be unique to entire application.
+///
+/// ## Usage Examples: ##
+/// ````
+/// var myLocalPreference = SimpleUserDefaults<String>(name: "myLocalPreference", defaultValue: "none")
+/// var myLocalImage = SimpleUserDefaults<NSURL>(name: "myLocalImage")
+/// var myLocalToken = SimpleUserDefaults<CKServerChangeToken>(name: "myLocalToken")
+///
+/// let preference: String? = myLocalPreference.invalidateCache().get() // returns "none"
+///
+/// let changeToken: CKServerChangeToken = tokenFetchedFromCKCompletionBlock
+/// myLocalToken.set(changeToken)
+public struct SimpleUserDefaults<Type> {
+    let name: String
+    var value: Type?
+    var defaultValue: Type?
+    var hasCachedValue: Bool = false
+    
+    /// Creates a cacheable UserDefaults entry of generic type.
+    ///
+    /// - Parameter name: The key to use when storing to UserDefaults. Should be unique.
+    /// - Parameter defaultValue: A default for when there is no stored data, or stored data is nil.
+    public init(name: String, defaultValue: Type? = nil) {
+        self.name = name
+        self.defaultValue = defaultValue
+    }
+    
+    /// Clears any prior cached values. (Chainable)
+    ///
+    /// - Returns: this SimpleUserDefaults object
+    public mutating func invalidateCache() -> SimpleUserDefaults {
+        value = nil
+        hasCachedValue = false
+        return self
+    }
+    
+    /// Retrieves the current UserDefaults stored value.
+    ///
+    /// Returns:
+    /// 1. The cached value, if found.
+    /// 2. The stored UserDefaults value, if found.
+    /// 3. The default value, if found.
+    /// 4. nil.
+    public mutating func get() -> Type? {
+        if !hasCachedValue {
+            if defaultValue is NSURL {
+                hasCachedValue = true
+                value = UserDefaults.standard.url(forKey: name) as? Type
+            } else if defaultValue is Bool {
+                hasCachedValue = true
+                value = UserDefaults.standard.bool(forKey: name) as? Type
+            } else if defaultValue is Int {
+                hasCachedValue = true
+                value = UserDefaults.standard.integer(forKey: name) as? Type
+            } else if defaultValue is Double {
+                hasCachedValue = true
+                value = UserDefaults.standard.double(forKey: name) as? Type
+            } else if defaultValue is Float {
+                hasCachedValue = true
+                value = UserDefaults.standard.float(forKey: name) as? Type
+            } else if defaultValue is String {
+                hasCachedValue = true
+                value = UserDefaults.standard.string(forKey: name) as? Type
+            } else {
+                // we don't always have to archive Array and Dictionary
+                // but this is easier
+                hasCachedValue = true
+                if let data = UserDefaults.standard.object(forKey: name) as? Data {
+                    value = (NSKeyedUnarchiver.unarchiveObject(with: data) as? Type)
+                }
+            }
+        }
+        return value ?? defaultValue
+    }
+    
+    /// Stores a new value to UserDefaults and caches it locally.
+    ///
+    /// - Parameter value: The new value to store.
+    public mutating func set(_ value: Type?) {
+        self.value = value
+        hasCachedValue = true
+        if defaultValue is NSURL
+            || defaultValue is Bool
+            || defaultValue is Int 
+            || defaultValue is Double
+            || defaultValue is Float 
+            || defaultValue is String {
+            hasCachedValue = true
+            if let value = value {
+                UserDefaults.standard.set(value, forKey: name)
+            } else {
+                UserDefaults.standard.set(nil, forKey: name)
+            }
+        } else {
+            hasCachedValue = true
+            if let value = value {
+                UserDefaults.standard.set(
+                    NSKeyedArchiver.archivedData(withRootObject: value),
+                    forKey: name
+                )
+            } else {
+                UserDefaults.standard.set(nil, forKey: name)
+            }
+        }
+    }
+}
+
+
+
+
+// Example of using enums for access:
+//public enum UserDefaultsKey: String {
+//    case myStoredPreference = "MyStoredPreference"
+//}
+//
+//extension SimpleUserDefaults {
+//    // OMG we can put init() in extensions now! I <3 Swift 3
+//    public init(name: UserDefaultsKey, defaultValue: Type? = nil) {
+//        self.name = name.rawValue
+//        self.defaultValue = defaultValue
+//    }
+//}
+
